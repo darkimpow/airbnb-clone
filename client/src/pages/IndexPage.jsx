c8c50fc3183ec4db4ff9f11c71dd0c24305ede0d
darkimpow <darkimpowell@gmail.com>
2023-04-20 17:09:27 -0400

    created header page for routes. Also created layout, login and index files

---
+++ client/src/pages/IndexPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+import Header from "../Header.jsx";
+
+function IndexPage() {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+}
+
+export default IndexPage;
